4a76f288c238e665500fe075c30bb2e3cd1b55fd
Iarlen <iarlensr@hotmail.com>
2024-05-18 00:25:42 -0300

    refactor: changing structure of the Navigation component.

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -3,13 +3,13 @@
     "title",
 ])
 
-<ul class="flex items-center gap-2 mt-6">
-    <li class="rounded-full bg-blue-400/10 p-1">
+<a
+    href="{{ $href }}"
+    class="flex w-fit items-center gap-2 text-lg transition-opacity hover:opacity-85"
+    wire:navigate
+>
+    <div class="rounded-full bg-blue-400/10 p-1">
         <x-gmdi-chevron-left-o class="w-6 text-white" />
-    </li>
-    <li>
-        <a href="{{ $href }}" class="text-lg" wire:navigate>
-            {{ $title }}
-        </a>
-    </li>
-</ul>
+    </div>
+    {{ $title }}
+</a>

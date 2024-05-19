4701897b3c508e65735195c383cdb9c970da3b35
Iarlen <iarlensr@hotmail.com>
2024-05-18 22:32:02 -0300

    refactor: wrapping the BettingCard component in a link that leads to the bet detail.

--- resources/views/components/betting-card.blade.php
+++ resources/views/components/betting-card.blade.php
@@ -1,12 +1,18 @@
 @props([
-    "bet"
+    "bet",
 ])
 
-<div class="flex flex-col gap-3 rounded bg-blue-400/10 px-2 py-3 xl:px-4">
+<a
+    wire:navigate
+    href="/aposta/{{ $bet->id }}"
+    class="flex flex-col gap-3 rounded bg-blue-400/10 px-2 py-3 shadow shadow-white/5 transition-opacity hover:opacity-85 xl:px-4"
+>
     <div class="flex items-start justify-between">
         <div class="flex flex-col gap-1">
-            <span class="text-sm line-clamp-1 md:text-lg xl:text-xl">{{ $bet->name }}</span>
-            <p class="text-xs md:text-sm text-white/60 xl:text-lg">
+            <span class="line-clamp-1 text-sm md:text-lg xl:text-xl">
+                {{ $bet->name }}
+            </span>
+            <p class="text-xs text-white/60 md:text-sm xl:text-lg">
                 {{ date("d M, Y", strtotime($bet->created_at)) }}
             </p>
         </div>
@@ -14,7 +20,7 @@
             <div
                 {{
                     $attributes->class([
-                        "h-2 w-2 md:h-2.5 md:w-2.5 animate-pulse rounded-full",
+                        "h-2 w-2 animate-pulse rounded-full md:h-2.5 md:w-2.5",
                         "bg-green-400" => $bet->status === "green",
                         "bg-red-400" => $bet->status === "red",
                         "bg-blue-400" => $bet->status === "pedding",
@@ -25,22 +31,31 @@
             ></div>
         </div>
     </div>
-    <div class="flex items-center justify-between text-xs md:text-sm xl:text-base">
+    <div
+        class="flex items-center justify-between text-xs md:text-sm xl:text-base"
+    >
         <span class="line-clamp-1">{{ $bet->market }}</span>
-        <span class="line-clamp-1">{{ number_format($bet->odds, 2, '.', '') }}</span>
-    </div>
-    <div class="flex items-center justify-end">
-        <span {{
-                    $attributes->class([
-                        "text-xs md:text-sm xl:text-base",
-                        "text-green-400" => $bet->status === "green",
-                        "text-red-400" => $bet->status === "red",
-                        "text-blue-400" => $bet->status === "pedding",
-                        "text-white" => $bet->status === "void",
-                    ])
-                }}>{{ $bet->status === "red"
-                ? "-R$ " . number_format($bet->amount, 2, ',', '.')
-                : "R$ " . number_format($bet->result, 2, ',', '.') }}
+        <span class="line-clamp-1">
+            {{ number_format($bet->odds, 2, ".", "") }}
         </span>
     </div>
-</div>
+    <div class="flex items-center justify-end">
+        <span
+            {{
+                $attributes->class([
+                    "text-xs md:text-sm xl:text-base",
+                    "text-green-400" => $bet->status === "green",
+                    "text-red-400" => $bet->status === "red",
+                    "text-blue-400" => $bet->status === "pedding",
+                    "text-white" => $bet->status === "void",
+                ])
+            }}
+        >
+            {{
+                $bet->status === "red"
+                    ? "-R$ " . number_format($bet->amount, 2, ",", ".")
+                    : "R$ " . number_format($bet->result, 2, ",", ".")
+            }}
+        </span>
+    </div>
+</a>
